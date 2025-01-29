f13692828dea42bb0f5bba0e387a0376c35f1ec8
chandra-samal <192883287+chandra-samal@users.noreply.github.com>
2025-01-29 20:18:36 +0530

    majority element

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(){
     int n = 6;
-    int myArr[n] = {13, 46, 24, 52, 20, 9};
+    int myArr[6] = {13, 46, 24, 52, 20, 9};
 
     // The complexity through this wil come out to be n^2
     for (int j = 0; j < n - 1; j++){
---
+++ majorityElement.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int majorityElement(vector<int>& nums){
+    map<int, int> mpp;
+    int n = nums.size();
+    for (int i = 0; i<n; i++){
+        if (mpp.find(nums[i]) != mpp.end()){
+            mpp[nums[i]]++;
+        }
+        else mpp[nums[i]] = 1;
+    }
+    for (int i = 0; i<n; i++){
+        if (mpp[nums[i]]>n/2) return nums[i];
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
+    cout << majorityElement(nums);
+
+    return 0;
+}
